a6ca42e905976691c256222700fb0b775feff359
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2021-05-19 01:27:55 +0300

    assisting enabled in DM

--- commands/helper/end-assist.js
+++ commands/helper/end-assist.js
@@ -4,14 +4,14 @@ const logger = require('../../utils/logger');
 module.exports = {
     name: 'end-assist',
     description: 'Ends interact with bot',
-    guildOnly: true,
+    guildOnly: false,
     args: false,
-    aliases: [],
+    aliases: ['end-chat'],
     usage: '',
     category: 'Helper',
     type: 'general',
     execute(message, args) {
-        logger.info(`End-Assist command has been used at guild:${message.guild.id} by:${message.author.id}`);
+        logger.info(`End-Assist command has been used at guild:${message.guild ? message.guild.id : 'DM'} by:${message.author.id}`);
         const client = message.client;
 
         const assistObject = client.assists.get(message.author.id);
