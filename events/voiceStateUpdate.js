fd9377587cfa4f4926b99315eb1b216dbdb308c6
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2021-05-21 00:45:32 +0300

    leave voice when no one in channel

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,21 @@
+const logger = require('../utils/logger');
+
+module.exports = {
+    name: 'voiceStateUpdate',
+    execute: async function(oldState, newState) {
+
+        if (oldState.channelID !==  oldState.guild.me.voice.channelID || newState.channel)
+            return;
+
+        if (oldState.channel.members.size === 1){
+            setTimeout(() => {
+                if (!oldState.channel.members.size - 1) {
+                    logger.debug("I am leaving voice")
+                    oldState.channel.leave();
+                }
+            }, 300000);
+        }
+
+
+    }
+};
