d6eecea55427fedeaa53079c6372f9296b2a3056
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2021-05-22 00:59:14 +0300

    inite command added

---
+++ commands/moderation/invite.js
@@ -0,0 +1,25 @@
+const logger = require('../../utils/logger');
+
+
+module.exports = {
+    name: 'invite',
+    description: 'Creates an invite link',
+    guildOnly: true,
+    args: false,
+    aliases: [],
+    usage: '',
+    permissions: 'CREATE_INSTANT_INVITE',
+    category: 'Moderation',
+    type: 'general',
+    execute(message, args) {
+        logger.debug(`Invite command has been used at guild:${message.guild.id} by:${message.author.id}`);
+
+        const channel = message.channel;
+
+        channel.createInvite()
+            .then(invite => {
+                channel.send(`Here is your invite link: ${invite.url}`);
+            })
+            .catch(console.error);
+    }
+};
