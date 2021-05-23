73f07c815f0f2558883a1197569c841d57e512b5
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2021-05-24 00:59:21 +0300

    new commands added

---
+++ commands/info/user.js
@@ -0,0 +1,25 @@
+const logger = require('../../utils/logger');
+const embed = require('../../utils/embeds').user;
+const api = require('../../utils/api');
+
+
+module.exports = {
+    name: 'user',
+    description: 'Shows tagged user information.',
+    guildOnly: true,
+    args: true,
+    aliases: [],
+    usage: '<user>',
+    category: 'Info',
+    type: 'general',
+    cooldown: 300,
+    execute: async function(message, args) {
+        logger.debug(`Me command has been used at guild:${message.guild.id} by:${message.author.id}`);
+        const member = message.guild.member(message.mentions.users.first());
+        if (member) {
+            const result = await api.getMember(message.guild.id, member.id);
+            return message.reply(embed(member, result));
+        }
+
+    }
+};
---
+++ commands/moderation/clone.js
@@ -0,0 +1,25 @@
+const logger = require('../../utils/logger');
+
+
+module.exports = {
+    name: 'clone',
+    description: 'Creates a clone of current text channel',
+    guildOnly: true,
+    args: false,
+    aliases: [],
+    usage: '',
+    permissions: 'MANAGE_CHANNELS',
+    category: 'Moderation',
+    type: 'general',
+    execute(message, args) {
+        logger.debug(`Clone command has been used at guild:${message.guild.id} by:${message.author.id}`);
+
+        try {
+            message.channel.clone();
+            message.channel.send(`A clone of ${message.channel} is created`);
+        }
+        catch (e) {
+            logger.error(e.message);
+        }
+    }
+};
