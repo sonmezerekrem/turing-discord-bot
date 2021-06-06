f925f062410ee52b632422c73b186114231577af
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2021-06-06 17:16:14 +0300

    messages for presence changes

---
+++ events/presenceUpdate.js
@@ -0,0 +1,20 @@
+const logger = require('../utils/logger');
+const api = require('../utils/api');
+const { presenceUpdate } = require('../utils/embeds');
+
+
+module.exports = {
+    name: 'presenceUpdate',
+    async execute(oldPresence, newPresence) {
+        logger.debug(`Member presence is changed ${newPresence.member.id}`);
+        if (newPresence && newPresence.activities.length > 0 && newPresence.activities[0].type === 'PLAYING') {
+            const member = await api.getMember(newPresence.guild.id, newPresence.member.id);
+            if (member && member.presenceUpdates.enabled) {
+                const channel = newPresence.guild.channels.get(member.presenceUpdates.channel);
+                if (channel) {
+                    channel.send(presenceUpdate(newPresence));
+                }
+            }
+        }
+    }
+};
